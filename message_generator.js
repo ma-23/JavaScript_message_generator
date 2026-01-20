417dda15d828e015c4ad2dd449bc570722da8bf0
mahmoud <mahmoodabuhassnah@gmail.com>
2026-01-20 14:28:55 +0300

    Created the random_generator

---
+++ message_generator.js
@@ -0,0 +1,8 @@
+//first create a  function that will select a random value from zero up to but not including  that number
+function random_generator(num){
+
+return Math.floor(Math.random()*num)
+
+
+}
+
