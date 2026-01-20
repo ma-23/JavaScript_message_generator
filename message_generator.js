f4031e1a3b4f535da023af4695e28e7e94db6f37
mahmoud <mahmoodabuhassnah@gmail.com>
2026-01-20 15:40:14 +0300

    Did the message generator function

--- message_generator.js
+++ message_generator.js
@@ -16,3 +16,19 @@ purpose:["coding","writing","answering",'video genration',"creating memes"]
 
 };
 
+const message_generator = (repo) => {
+let words = [];
+for(let i in repo){
+let random_index = random_generator(repo[i].length);
+
+let word = repo[i][random_index];
+
+words.push(word);
+
+
+}
+
+console.log(`${words[0]} is ${words[1]} at ${words[2]}.`);
+};
+
+message_generator(repo);
