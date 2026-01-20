9734899621e413a45831831328a386e81f79ffcb
mahmoud <mahmoodabuhassnah@gmail.com>
2026-01-20 14:45:13 +0300

    Creataed the repo object

--- message_generator.js
+++ message_generator.js
@@ -6,3 +6,13 @@ return Math.floor(Math.random()*num)
 
 }
 
+//Create the object with three array properties. 
+const repo =  {
+ai: ["Grok","ChatGPT","Gemini","Claude"],
+adjactive:["good","bad","amazing","horrible","great","garbage","fantastic"],
+purpose:["coding","writing","answering",'video genration',"creating memes"]
+
+
+
+};
+
